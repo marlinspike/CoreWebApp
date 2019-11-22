761909cfaf9a8c98cb72174b37aab2702d20f180
Reuben Cleetus <recleetu@microsoft.com>
2019-11-22 14:27:01 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoreWebApp.Models;
+
+namespace CoreWebApp.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            _logger.LogInformation("Log message in the Index() method");
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            _logger.LogInformation("Log message in the Privacy() method");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            _logger.LogError("Log message in the Error() method");
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
